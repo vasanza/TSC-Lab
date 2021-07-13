583e63d294a527f04c8b018d4abaf07c04276ba5
vasanza <vasanza@gmail.com>
2021-07-12 22:53:45 -0500

    Update Practice4_code.ino
    // TSC-LAB
    // Blog: https://tsc-lab.blogspot.com
    // More examples: https://github.com/vasanza/TSC-Lab
    // Dataset: http://ieee-dataport.org/4138

--- Practice4/Practice4_code/Practice4_code.ino
+++ Practice4/Practice4_code/Practice4_code.ino
@@ -9,7 +9,10 @@
     • Case 3: Activation of Transistor 2 and Reading of temperature sensor 1 and 2
 
    By: Kevin E. Chica O
+   Reviewed: Víctor Asanza
    More information: https://tsc-lab.blogspot.com/
+   More examples: https://github.com/vasanza/TSC-Lab
+   Dataset: http://ieee-dataport.org/4138
 */
 
 #include <OneWire.h>
