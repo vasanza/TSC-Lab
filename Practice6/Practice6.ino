c9ed40d041ad44eb5836822ff676ebbeae14c3a9
vasanza <vasanza@gmail.com>
2021-07-12 22:55:05 -0500

    Update Practice6.ino
    // TSC-LAB
    // Blog: https://tsc-lab.blogspot.com
    // More examples: https://github.com/vasanza/TSC-Lab
    // Dataset: http://ieee-dataport.org/4138

--- Practice6/Practice6.ino
+++ Practice6/Practice6.ino
@@ -3,7 +3,10 @@
    ***************************** PRACTICE 6 *****************************
    This practice is about initial setups and tests (ON/OFF) of speed control lab
    By: Kevin E. Chica O
+   Reviewed: Víctor Asanza
    More information: https://tsc-lab.blogspot.com/
+   More examples: https://github.com/vasanza/TSC-Lab
+   Dataset: http://ieee-dataport.org/4138
 */
 
 //motor
